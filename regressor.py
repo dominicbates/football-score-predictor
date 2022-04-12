ef4c07802b6809deb07056e8bb6c99e7d45f7e99
Dominic Bates <thisisdom23@googlemail.com>
2022-04-13 00:59:03 +0100

    Added very innefficient goals simulator

--- regressor.py
+++ regressor.py
@@ -21,17 +21,43 @@ class PoissonRegressor:
             self.fit_intercept = config['fit_intercept']
         self.features = config['features']
         self.target = config['target']    
+        
         self.model = linear_model.PoissonRegressor(alpha=0, fit_intercept=self.fit_intercept)
         self.model_params = None
         self.model_intercept = None
     
+
     def train(self, df):
         df = df[df['f|played'] == 1]
         self.model.fit(df[self.features], df[self.target])
         self.model_params = dict(zip(self.features,self.model.coef_))
         self.model_intercept = self.model.intercept_
 
+
     def predict(self, df):
         return self.model.predict(df[self.features])
+    
+
+    def generate_goals_pdf(self, df, max_goals = 10, n_draws = 10000):
+    
+        df = df.reset_index(level=None, drop=True) # Incase index is not 0,1,2...
+        ### SUPER INNEFFICIENT AND NOT EXACT - DO IT ANALYTICALLY
+        prediction_means = self.predict(df)
+        
+        # Create blank columns
+        for g in range(max_goals+1):
+            df['p|prob_'+str(g)+'_goals'] = 0
+        
+        # Simulate n_goals for each match ('n_draws' times)
+        for n in range(len(prediction_means)):
+            simulated_goals = np.random.poisson(prediction_means[n], size=n_draws)
+            
+            for g in range(max_goals+1):
+                prob_g = (simulated_goals == g).sum()/n_draws # Number of draws with n_goals = g
+                df.loc[n,'p|prob_'+str(g)+'_goals'] = prob_g
+
+         # Return oroginal dataframe with extrac columns
+         return df
 
 
+         
