277aad1548b35bda27637c223ec4fd34e1bfc5b7
Dominic Bates <thisisdom23@googlemail.com>
2022-04-18 17:18:59 +0100

    Added most likely scored to output

--- regressor.py
+++ regressor.py
@@ -1,5 +1,6 @@
 from sklearn import linear_model
 import numpy as np
+import pandas as pd
 
 
 class PoissonRegressor:
@@ -24,26 +25,26 @@ class PoissonRegressor:
         
         self.model = linear_model.PoissonRegressor(alpha=0, fit_intercept=self.fit_intercept)
         self.model_params = None
+        self.model_params_split = {}
         self.model_intercept = None
+        self.model_fit = False
     
-
     def train(self, df):
         df = df[df['f|played'] == 1]
         self.model.fit(df[self.features], df[self.target])
         self.model_params = dict(zip(self.features,self.model.coef_))
         self.model_intercept = self.model.intercept_
-
+        self.model_fit = True
 
     def predict(self, df):
         return self.model.predict(df[self.features])
-    
     
     def print_params_pretty(self):
         if self.model_fit == True:
             
             df = pd.DataFrame()
-            df['feature'] = list(model.model_params)
-            df['value'] = model.model_params.values()
+            df['feature'] = list(self.model_params)
+            df['value'] = self.model_params.values()
             attack = df[df['feature'].str.contains('f\|team')].sort_values('value',ascending=False)
             defence = df[df['feature'].str.contains('f\|opp')].sort_values('value',ascending=True)
             other = df[df['feature'].str.contains('f\|home')].sort_values('value',ascending=True)
@@ -53,10 +54,8 @@ class PoissonRegressor:
             print(defence.set_index('feature'))
             print('\nOther features:')
             print(other.set_index('feature'))
-
-
-
-    def generate_match_preds(self, df, max_goals = 10, n_draws = 10000):
+            
+    def generate_match_preds(self, df, max_goals = 10, n_draws = 10000, n_scores_keep = 10):
 
         # Generate preds
         prediction_means = self.predict(df)
@@ -68,9 +67,8 @@ class PoissonRegressor:
         all_matches = list(set(df['match_id']))
         for m_id in all_matches:
             
-            m_id_str = str(int(m_id))
-            
             # Blank dict
+            m_id_str = str(int(m_id))
             match_preds[m_id_str] = {}
             
             # Rows
@@ -89,7 +87,7 @@ class PoissonRegressor:
             simulated_goals_h = np.random.poisson(prediction_means[m_h], size=n_draws)
             simulated_goals_a = np.random.poisson(prediction_means[m_a], size=n_draws)
 
-            # Win/draw/loss probs
+            # Win probs
             match_preds[m_id_str]['p|home_win'] = (simulated_goals_h>simulated_goals_a).sum() / n_draws
             match_preds[m_id_str]['p|draw'] = (simulated_goals_h==simulated_goals_a).sum() / n_draws
             match_preds[m_id_str]['p|away_win'] = (simulated_goals_h<simulated_goals_a).sum() / n_draws
@@ -101,8 +99,25 @@ class PoissonRegressor:
                 match_preds[m_id_str]['p|home_goals'][str(g)] = (simulated_goals_h == g).sum()/n_draws
                 match_preds[m_id_str]['p|away_goals'][str(g)] = (simulated_goals_a == g).sum()/n_draws
                 
+            # Most likely scores
+            match_preds[m_id_str]['p|scores'] = {}
+            for h in match_preds[m_id_str]['p|home_goals']:
+                for a in match_preds[m_id_str]['p|home_goals']:
+                    match_preds[m_id_str]['p|scores'][str(h)+'-'+str(a)] = (match_preds[m_id_str]['p|home_goals'][h] *
+                                                                            match_preds[m_id_str]['p|away_goals'][a])
+            # Only keep top N
+            scores_df = pd.DataFrame({'scores': list(match_preds[m_id_str]['p|scores']),
+                          'probs': list(match_preds[m_id_str]['p|scores'].values())}).sort_values('probs',ascending=False)
+            match_preds[m_id_str]['p|scores'] = {}
+            for n in range(n_scores_keep):
+                match_preds[m_id_str]['p|scores'][scores_df['scores'].iloc[n]] = scores_df['probs'].iloc[n]
+            
+            
         return match_preds
     
+    
+   
+
     
 
 
