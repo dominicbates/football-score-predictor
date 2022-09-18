7e9010f39f9a7c6a55498270e811338d85ca3c14
dominicbates <43854430+dominicbates@users.noreply.github.com>
2022-09-18 13:16:29 +0100

    Added all steps to production run (apart from final to-dataframe transformation)

--- production_run.py
+++ production_run.py
@@ -1,2 +1,24 @@
-import football_score_predictor.data_extractor as data_extractor
-import football_score_predictor.regressor as regressor
+import data_extractor as data_extractor
+import regressor as regressor
+
+print('\nGetting production data...')
+production_df = data_extractor.get_production_data(n_years_minus=2, n_weeks_plus=8)
+print('Data exracted!')
+
+print('\nSetting config and model features based on current production data...')
+features = [f for f in list(production_df) if ('f|team' in f) or ('f|opp' in f) or ('f|home' in f)]
+production_config = {'fit_intercept':False,
+          'target':'p|goals|scored',
+          'features':features}
+print('Config set!')
+
+print('Training model...')
+production_model = regressor.Regressor(config = production_config)
+production_model.train(production_df)
+print('Model trained!')
+
+print('Generating preductions for all games...')
+match_preds_dict = production_model.generate_match_preds()
+
+# NEED TO TURN TO FINAL OUTPUT FOTMAT
+print('Generating preductions for all games...')
