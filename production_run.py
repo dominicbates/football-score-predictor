c852a9b5ad4824d60afe9a1eef20a8b798c40c1c
Dominic Bates <dominicbates@Dominics-Air.home>
2022-09-20 01:25:22 +0100

    Changed hyperparameters of production run to some sentible values (i.e. recency weight and look back time)

--- production_run.py
+++ production_run.py
@@ -3,22 +3,29 @@ import regressor as regressor
 
 print('\nGetting production data...')
 production_df = data_extractor.get_production_data(n_years_minus=2, n_weeks_plus=8)
+print(production_df.head())
 print('Data exracted!')
 
 print('\nSetting config and model features based on current production data...')
 features = [f for f in list(production_df) if ('f|team' in f) or ('f|opp' in f) or ('f|home' in f)]
 production_config = {'fit_intercept':False,
           'target':'p|goals|scored',
-          'features':features}
+          'features':features,
+          'weight_col':'f|recency_weight'}
 print('Config set!')
 
 print('\nTraining model...')
-production_model = regressor.Regressor(config = production_config)
-production_model.train(production_df)
+production_model = regressor.PoissonRegressor(config = production_config)
+production_model.train(production_df, recency_weight=1.5)
 print('Model trained!')
 
 print('\nGenerating preductions for all games...')
-match_preds_dict = production_model.generate_match_preds()
-
+match_preds_dict = production_model.generate_match_preds(production_df)
+for m in match_preds_dict:
+    print(match_preds_dict[m])
+    break
+match_preds_df = regressor.match_preds_to_df(match_preds_dict)
+print(match_preds_df.head())
+match_preds_df.to_csv('test_football_output.csv')
 # NEED TO TURN TO FINAL OUTPUT FOTMAT
 print('Generating preductions for all games...')
