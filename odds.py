04108c928ecad0943afdd0fe51ad803a0d571925
Dominic Bates <dominicbates@Dominics-Air.home>
2022-09-23 12:58:23 +0100

    Added odds package to append 888 sport odds to output and calculate under/over valued odds

---
+++ odds.py
@@ -0,0 +1,59 @@
+import pandas as pd
+from soccerapi.api import Api888Sport
+
+
+def get_odds():
+
+	# Extract odds
+	api = Api888Sport()
+	url = api.competitions()['England']['Premier League']
+	current_odds = api.odds(url)
+
+	# Create dict
+	odds_dict = {'date':[],
+	        'home_team':[],
+	        'away_team':[],
+	        'home_odds':[],
+	        'draw_odds':[],
+	        'away_odds':[]
+	}
+
+	# Turn int to odds
+	def transform_odds(n):
+	    return (n-1000)/1000
+
+	# Add to dataframe
+	for game in current_odds:
+	    odds_dict['date'].append(game['time'])
+	    odds_dict['home_team'].append(game['home_team'].lower().replace(' ','_').replace('wolves','wolverhampton'))
+	    odds_dict['away_team'].append(game['away_team'].lower().replace(' ','_').replace('wolves','wolverhampton'))
+	    odds_dict['home_odds'].append(transform_odds(game['full_time_result']['1']))
+	    odds_dict['draw_odds'].append(transform_odds(game['full_time_result']['X']))
+	    odds_dict['away_odds'].append(transform_odds(game['full_time_result']['2']))
+	odds_df = pd.DataFrame(odds_dict)
+
+	return odds_df
+
+
+
+def append_odds_to_df(final_df, odds_df):
+
+	# Loop through and append odds (simple join doesn't work e.g. 'west_ham' vs 'west_ham_united')
+	for n in range(len(final_df)):
+	    for m in range(len(odds_df)):
+	        if (odds_df['home_team'][m] in final_df['home_team'][n]) and (odds_df['away_team'][m] in final_df['away_team'][n]):
+	            
+	            # Append odds
+	            final_df.loc[n,'odds|home_win'] = odds_df.loc[m,'home_odds']
+	            final_df.loc[n,'odds|draw'] = odds_df.loc[m,'draw_odds']
+	            final_df.loc[n,'odds|away_win'] = odds_df.loc[m,'away_odds']
+	            
+	            # Calculate expected innings
+	            final_df.loc[n,'p|winnings|home_win'] = final_df.loc[n,'p|home_win']* (1+final_df.loc[n,'odds|home_win'])
+	            final_df.loc[n,'p|winnings|draw'] = final_df.loc[n,'p|draw']* (1+final_df.loc[n,'odds|draw'])
+	            final_df.loc[n,'p|winnings|away_win'] = final_df.loc[n,'p|away_win']* (1+final_df.loc[n,'odds|away_win'])
+
+	return final_df
+
+
+
--- production_run.py
+++ production_run.py
@@ -1,34 +1,46 @@
 import data_extractor as data_extractor
 import regressor as regressor
+import odds as odds
+
+
+##### Change to argument in code using argparse?
+output_fname = 'football_betting_output.csv'
+#####
 
 print('\nGetting production data...')
-production_df = data_extractor.get_production_data(n_years_minus=2, n_weeks_plus=8)
-print(production_df.head())
+production_df = data_extractor.get_production_data(n_weeks_minus=52, n_weeks_plus=8)
 print('Data exracted!')
 
+
 print('\nSetting config and model features based on current production data...')
-features = [f for f in list(production_df) if ('f|team' in f) or ('f|opp' in f) or ('f|home' in f)]
+features = [f for f in list(production_df) if ('f|team' in f) or ('f|opp' in f)]# or ('f|home' in f)]
 production_config = {'fit_intercept':False,
           'target':'p|goals|scored',
           'features':features,
-          'weight_col':'f|recency_weight'}
+          'weight_col':'f|recency_weight',
+          'weight_power':4.5} # This plus n_weeks_minus=52 is best set of hyperparameters
 print('Config set!')
+
 
 print('\nTraining model...')
 production_model = regressor.PoissonRegressor(config = production_config)
-production_model.train(production_df, recency_weight=1.5)
+production_model.train(production_df)
 print('Model trained!')
+production_model.print_params_pretty()
+
 
 print('\nGenerating preductions for all games...')
-match_preds_dict = production_model.generate_match_preds(production_df)
-for m in match_preds_dict:
-    print(match_preds_dict[m])
-    break
+match_preds_dict = production_model.generate_match_preds(production_df, force_home_impact=0.181841)
 match_preds_df = regressor.match_preds_to_df(match_preds_dict)
-print(match_preds_df.head())
-# match_preds_df.to_csv('test_football_output.csv')
-match_preds_df[['date', 'home_team','away_team','played','p|home_win','p|draw','p|away_win']].to_csv('test_football_betting_output.csv')
+match_preds_df = match_preds_df[['date', 'home_team','away_team','played','p|home_win','p|draw','p|away_win','p|winnings|home_win','p|winnings|draw','p|winnings|away_win','odds|home_win','odds|draw','odds|away_win','true|home_win','true|draw','true|away_win','p|score_1|score','p|score_1|prob','p|score_2|score','p|score_2|prob','p|score_3|score','p|score_3|prob','p|score_4|score','p|score_4|prob','p|score_5|score','p|score_5|prob']]
+print('Generated')
 
-# NEED TO TURN TO FINAL OUTPUT FOTMAT
-print('Generated and saved!')
-print('\n\nUse output to calculate ratio (i.e. expected winning fraction on bet, >1 is good)\nCalculate like: "ratio = true_probability*(1+odds)"')
+
+print('\nGetting odds for future games and appending')
+odds_df = odds.get_odds()
+match_preds_df = odds.append_odds_to_df(match_preds_df, odds_df)
+match_preds_df.to_csv(output_fname)
+print('Appended and saved as:',output_fname)
+
+
+print('\nUse output to calculate expected winnings ratio (i.e. fraction won on bet on average, >1 is good)\nCalculate like: "ratio = true_probability*(1+odds)"')
